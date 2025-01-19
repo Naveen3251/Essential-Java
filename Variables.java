776c49b74b3d8fabbe20135428af9b81b02fd338
Naveen3251 <naveensakthivel3251@gmail.com>
2025-01-19 18:45:39 +0530

    variables

---
+++ Variables.java
@@ -0,0 +1,26 @@
+/*
+Integrals - byte 1, short 2, int 4, long 8
+floating point- float 4, double 8
+char 2
+boolean
+*/
+/*
+checking methods available in particular class
+eg:
+javap java.lang.Integer
+ */
+
+class Variables {
+    public static void main(String args[]){
+        byte b=127;
+        short s= 130;
+        int i = 19082;
+        long l=19820830;
+        float f=123.4f;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+    }
+}
